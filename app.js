3b86e9fc07e483c38726c9530142e807584ccdb9
Alex Koenig <alex.koenig4@gmail.com>
2021-01-02 01:12:58 -0500

    Level 5: Implement passport local strategy
    Replaced bcrypt with passport local strategy and passport local
    mongoose packages for authentication encryption and session
    management.

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const { allowedNodeEnvironmentFlags } = require('process');
 
 const app = express();
 
@@ -15,14 +17,31 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.use(session({
+    secret: "Nobody will ever figure this out.",
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 })
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema)
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -33,24 +52,33 @@ app.get('/login', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-    const username = req.body.username
-    const password = req.body.password
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
 
-    User.findOne({ email: username }, (err, foundUser) => {
+    req.login(user, (err) => {
         if(err) {
-            console.log(err)
+            console.log(err);
         } else {
-            if(foundUser) {
-                bcrypt.compare(password, foundUser.password, (err, result) => {
-                    if(result === true) {
-                        res.render('secrets')
-                    } else {
-                        console.log(err)
-                    }
-                })
-            }
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/secrets");
+            })
         }
-    })
+    });
+})
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect("/");
+})
+
+app.get('/secrets', (req, res) => {
+    if (req.isAuthenticated) {
+        res.render('secrets');
+    } else {
+        res.redirect("/login");
+    }
 })
 
 app.get('/register', (req, res) => {
@@ -58,21 +86,16 @@ app.get('/register', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        })
-    
-        newUser.save((err) => {
-            if(err) {
-                console.log(err)
-            } else {
-                res.render('secrets')
-            }
-        })
-    }) 
-})
+    User.register({username: req.body.username}, req.body.password, (err, user) => {
+        if(err) {
+            console.log(err);
+        } else {
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/secrets");
+            });
+        }
+    });
+});
 
 app.listen(4000, function() {
     console.log("Server started on port 4000...");
