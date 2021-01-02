26c7cbdd266e86681ad67fcb8d4e6556bdaf9454
Alex Koenig <alex.koenig4@gmail.com>
2021-01-01 23:32:51 -0500

    Level 3: Implement hashing
    Removed encryption method/key and replaced with md5 hashing algorithm to
    secure user passwords

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs")
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
 const { setFlagsFromString } = require("v8");
+const md5 = require('md5');
 
 const app = express();
 
@@ -22,8 +22,6 @@ const userSchema = new mongoose.Schema ({
     password: String
 })
 
-userSchema.plugin(encrypt, { secret: process.env.ENCRYPTION_SECRET, encryptedFields: ['password'] })
-
 const User = new mongoose.model("User", userSchema)
 
 app.get('/', (req, res) => {
@@ -36,7 +34,7 @@ app.get('/login', (req, res) => {
 
 app.post('/login', (req, res) => {
     const username = req.body.username
-    const password = req.body.password
+    const password = md5(req.body.password)
 
     User.findOne({ email: username }, (err, foundUser) => {
         if(err) {
@@ -58,7 +56,7 @@ app.get('/register', (req, res) => {
 app.post('/register', (req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
 
     newUser.save((err) => {
