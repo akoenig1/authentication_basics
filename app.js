8f3be1367146d516b9ec9e5115b77be61197bb24
Alex Koenig <alex.koenig4@gmail.com>
2021-01-02 00:16:38 -0500

    Level 4: Implement salting with bcrypt
    Added bcrypt as dependency and included 10 rounds of salting
    before password storage and comparison

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs")
+const ejs = require("ejs");
 const mongoose = require("mongoose");
-const { setFlagsFromString } = require("v8");
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -34,16 +34,20 @@ app.get('/login', (req, res) => {
 
 app.post('/login', (req, res) => {
     const username = req.body.username
-    const password = md5(req.body.password)
+    const password = req.body.password
 
     User.findOne({ email: username }, (err, foundUser) => {
         if(err) {
             console.log(err)
         } else {
             if(foundUser) {
-                if(password === foundUser.password) {
-                    res.render('secrets')
-                }
+                bcrypt.compare(password, foundUser.password, (err, result) => {
+                    if(result === true) {
+                        res.render('secrets')
+                    } else {
+                        console.log(err)
+                    }
+                })
             }
         }
     })
@@ -54,18 +58,20 @@ app.get('/register', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
-
-    newUser.save((err) => {
-        if(err) {
-            console.log(err)
-        } else {
-            res.render('secrets')
-        }
-    })
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        })
+    
+        newUser.save((err) => {
+            if(err) {
+                console.log(err)
+            } else {
+                res.render('secrets')
+            }
+        })
+    }) 
 })
 
 app.listen(4000, function() {
