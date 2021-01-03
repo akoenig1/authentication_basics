8e59dd2debfd8d20a3ad6ec2643589f34537f01f
Alex Koenig <alex.koenig4@gmail.com>
2021-01-02 23:35:47 -0500

    Complete secret submission routes
    Added get and post routes for secret submission.
    Edited ejs template for secrets to find all users with submitted
    secrets and display those secrets (detached from the user)

--- app.js
+++ app.js
@@ -36,7 +36,8 @@ const userSchema = new mongoose.Schema ({
     email: String,
     password: String,
     googleId: String,
-    facebookId: String
+    facebookId: String,
+    secret: String
 })
 
 userSchema.plugin(passportLocalMongoose);
@@ -141,11 +142,40 @@ app.get('/logout', (req, res) => {
 })
 
 app.get('/secrets', (req, res) => {
+    User.find({'secret': {$ne: null}}, (err, foundUsers) => {
+        if(err) {
+            console.log(err);
+        } else {
+            if(foundUsers) {
+                res.render('secrets', {usersWithSecrets: foundUsers});
+            }
+        }
+    })
+})
+
+app.get('/submit', (req, res) => {
     if (req.isAuthenticated) {
-        res.render('secrets');
+        res.render('submit');
     } else {
         res.redirect('/login');
     }
+})
+
+app.post('/submit', (req, res) => {
+    const submittedSecret = req.body.secret;
+
+    User.findById(req.user.id, (err, foundUser) => {
+        if(err) {
+            console.log(err)
+        } else {
+            if(foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(() => {
+                    res.redirect('/secrets');
+                });
+            }
+        }
+    });
 })
 
 app.get('/register', (req, res) => {
